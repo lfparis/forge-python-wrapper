8ccd8d82084792f9a13e36b470032eea389a4a51
Luis Felipe Paris <lparis2@Luiss-MacBook-Pro.local>
2020-02-13 13:17:33 +0000

    Added examples

---
+++ examples/get_projects_bim360_team.py
@@ -0,0 +1,25 @@
+from forge import ForgeApp
+
+
+def main():
+
+    app = ForgeApp(three_legged=True)
+    hub_type = ForgeApp.TYPES[ForgeApp.BIM_360_TYPES["b."]]["hubs"]
+
+    app.get_hubs()
+    hub_id = [
+        hub["id"]
+        for hub in app.hubs
+        if hub["attributes"]["extension"]["type"] == hub_type
+    ][0]
+    app.hub_id = hub_id
+
+    app.get_projects(source="docs")
+
+    for project in app.projects:
+        print(project.name)
+        print(project.data)
+
+
+if __name__ == "__main__":
+    main()
