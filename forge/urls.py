66c02ac7c5fd98f37fd817d165a272fd8bade463
Luis Felipe Paris <lfparis@gmail.com>
2020-03-03 18:02:06 +0000

    added links

--- forge/urls.py
+++ forge/urls.py
@@ -6,13 +6,19 @@ https://forge.autodesk.com/en/docs/
 BASE_URL = "https://developer.api.autodesk.com"
 
 # Authentication (OAuth)
+# https://forge.autodesk.com/en/docs/oauth/v2/developers_guide/basics/
 AUTH_V1_URL = "{}/authentication/v1".format(BASE_URL)
 
 # BIM 360 API
+# https://forge.autodesk.com/en/docs/bim360/v1/overview/basics/
 HQ_V1_URL = "{}/hq/v1".format(BASE_URL)
 HQ_V2_URL = "{}/hq/v2".format(BASE_URL)
 
 # Data Management API
+# https://forge.autodesk.com/en/docs/data/v2/developers_guide/basics/
+# Data Service
 DATA_V1_URL = "{}/data/v1".format(BASE_URL)
+# Project Service
 PROJECT_V1_URL = "{}/project/v1".format(BASE_URL)
+# Object Storage Service
 OSS_V2_URL = "{}/oss/v2".format(BASE_URL)
